90b2a59d8e7799f0696758f4cf38300287b7e13c
Zach Hendershot <zach@hidefweb.com>
2013-08-11 16:29:32 -0600

    Tweaking to be ARC compatible

--- BrightBotSDK/BrightBot.m
+++ BrightBotSDK/BrightBot.m
@@ -392,13 +392,8 @@ void (^authFinish)(NSMutableDictionary*);
 }*/
 
 - (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning]; // Releases the view if it doesn't have a superview
+    //[super didReceiveMemoryWarning]; // Releases the view if it doesn't have a superview
     // Release anything that's not essential, such as cached data
-}
-
-- (void)dealloc {
-    [webView release];
-    [super dealloc];
 }
 
 @end
